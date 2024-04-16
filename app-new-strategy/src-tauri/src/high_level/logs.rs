28c1f8eb6550b528b983fed6afbfcb553dfc6728
ordinaryask <ordinaryask@gmail.com>
2024-04-16 17:12:09 +0200

    f

--- app-new-strategy/src-tauri/src/high_level/logs.rs
+++ app-new-strategy/src-tauri/src/high_level/logs.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
 
 #[tauri::command]
@@ -17,10 +17,23 @@ pub fn get_logs() -> Vec<String> {
 
 #[tauri::command]
 pub fn clear_logs(rows: Vec<String>) -> Result<(), String> {
-    let path = Path::new("logs.log");
-
-    let _file = match File::create(path) {
-        Ok(_) => return Ok(()),
-        Err(_) => return Err("Error clearing file".to_string()),
+    let file = match File::open("logs.log") {
+        Ok(file) => file,
+        Err(_) => return Err("Error opening file".to_string()),
     };
+
+    let reader = BufReader::new(file);
+    let mut lines: Vec<String> = reader.lines().filter_map(Result::ok).collect();
+
+    lines.retain(|line| !rows.contains(line));
+
+    let mut file = File::create("logs.log").unwrap();
+    for line in lines {
+        match writeln!(file, "{}", line) {
+            Ok(_) => {}
+            Err(_) => return Err("Error writing to file".to_string()),
+        };
+    }
+
+    Ok(())
 }
